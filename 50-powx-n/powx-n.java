a88ed9fbb5314946598c1323616554820e396546
Ramanaa <137044894+7177821l212@users.noreply.github.com>
2024-07-09 13:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (35.93%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // double result = 1.0;
+        // long new_n = n;
+        // if(new_n < 0)
+        //     new_n = -1 * n;
+        // for(int  i = 0; i < n; i++)
+        // {   
+             
+        //     result = result * x;
+           
+        // }
+       double result =  Math.pow(x,n);
+        return result;
+    }
+}
