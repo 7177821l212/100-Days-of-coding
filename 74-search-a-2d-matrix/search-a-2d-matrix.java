ebc22cba91b7f2d1b8e386fe660bd74699b81791
Ramanaa <137044894+7177821l212@users.noreply.github.com>
2024-07-22 21:52:52 +0530

    Added a comment line

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        //Brute force solution [TC:n2]!!
         int row = matrix.length;
         int colm = matrix[0].length;
-
         for(int  i = 0; i < row; i++)
         {    
             for(int j = 0; j < colm; j++){
